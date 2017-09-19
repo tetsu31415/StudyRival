b90d2828f1968204c2881249d15d30df51497489
tetsu31415 <tetsu31415dev@gmail.com>
2017-09-19 15:17:14 +0900

    Change directory structure Djangoのプロジェクトがレポジトリのルートに来るようにディレクトリ構成を変更

--- testapp/timer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.test_view, name='stop_watch'),
-#    url(r'^$', views.test_view, name='test'),
-]
