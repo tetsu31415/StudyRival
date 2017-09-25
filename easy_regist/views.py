b253648de219fcf4770574fc7b37d684d736db79
tetsu31415 <tetsu31415dev@gmail.com>
2017-09-25 16:03:44 +0900

    easy_regist内の不要なコードの削除

--- easy_regist/views.py
+++ easy_regist/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render
-from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.contrib.auth.tokens import default_token_generator
-from django.contrib.sites.shortcuts import get_current_site
-from django.core.mail import send_mail
 from django.core.urlresolvers import reverse_lazy
-from django.http import Http404
-from django.template.loader import get_template
-from django.utils.encoding import force_bytes, force_text
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.views import generic
  
 from .forms import UpdateForm
